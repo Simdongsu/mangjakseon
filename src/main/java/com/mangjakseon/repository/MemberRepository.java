dbfebe29cd4330f5f13cf9d54bdac42e68c69701
Simdongsu <102348698+Simdongsu@users.noreply.github.com>
2022-07-08 16:09:31 +0900

    Delete src directory

--- src/main/java/com/mangjakseon/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mangjakseon.repository;
-
-import com.mangjakseon.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
